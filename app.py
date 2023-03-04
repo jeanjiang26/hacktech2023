105a9ce3084bf47407905a5544c5b8ef811575e4
Jeannie Jiang <jeanniej@usc.edu>
2023-03-04 14:33:25 -0800

    deleted everything lol

--- app.py
+++
@@ -1,2 +0,0 @@
-# contain code to integrate the backend with the website
-# can use a Python web framework such as Flask or Django to build the web application and display the recommendations to users

cf0cda7fa37003c3fae1b66f89b990e2f1f62519
Jeannie Jiang <jeanniej@usc.edu>
2023-03-04 11:32:20 -0800

    commit api file

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
 # contain all the configuration variables for your project, such as API keys and access tokens. You can use this file to store sensitive information that you don't want to hardcode into your main Python files.
+
+# Authenticate with the SentinelHub API
+SENTINELHUB_API_KEY = "your-api-key-here"
+SENTINELHUB_INSTANCE_ID = "your-instance-id-here"
+
+# Authenticate with the GHGSat AI model
+GHGSAT_API_KEY = "your-api-key-here"
+GHGSAT_MODEL_ID = "your-model-id-here"
+
+# Green roof estimation settings
+# Specify the cost per square foot for installing a green roof
+GREEN_ROOF_COST_PER_SQ_FT = 20.0
+
+# stores the machine learning model used to estimate the suitable areas for green roofs, which in this case is set to "random-forest"
+GREEN_ROOF_ESTIMATION_MODEL = "random-forest"
+
+
--- sentinelhub-api.py
+++ sentinelhub-api.py
@@ -1,3 +1,65 @@
 # contain all the code needed to interact with the SentinelHub API
 # write functions to make API requests and process the responses
 # use the requests library to make HTTP requests to the API, and the json library to parse the responses
+
+
+# Import the required packages at the beginning of the file
+#  importing the necessary packages for using the SentinelHub API and working with dates and times
+import os
+from sentinelhub import SentinelHubRequest, BBox, DataCollection, \
+    SentinelHubDownloadClient, SentinelHubCatalog, SHConfig
+from datetime import datetime
+
+# Import the required configuration variables from config.py
+# Importing the SENTINELHUB_API_KEY and SENTINELHUB_INSTANCE_ID variables from config.py
+from config import SENTINELHUB_API_KEY, SENTINELHUB_INSTANCE_ID
+
+# Set up SentinelHub API credentials
+# Setting the instance ID and API key in the SHConfig instance
+config = SHConfig()
+config.instance_id = SENTINELHUB_INSTANCE_ID
+config.sh_client_id = ''
+config.sh_client_secret = ''
+config.auth_config = {'SH_CLIENT_ID': '', 'SH_CLIENT_SECRET': ''}
+config.api_key = SENTINELHUB_API_KEY
+
+# Define a function that takes a bounding box and a date range, and returns a list of Sentinel-2 images for that area and time period
+# A function that takes a bounding box, start date, and end date as input, and uses the SentinelHub API to download Sentinel-2 images for the specified area and time period
+# Images are saved in a local folder named sentinel_images, and the function returns a list of file paths for the downloaded images
+def get_sentinel_images(bbox, start_date, end_date):
+    """
+    Downloads Sentinel-2 images for the specified bounding box and date range.
+
+    :param bbox: A bounding box in the format (minx, miny, maxx, maxy).
+    :param start_date: A string representing the start date in ISO format (e.g. "2022-01-01").
+    :param end_date: A string representing the end date in ISO format (e.g. "2022-01-31").
+    :return: A list of file paths for the downloaded images.
+    """
+    # Create SentinelHub request
+    request = SentinelHubRequest(
+        data_folder='sentinel_images',
+        evalscript='(B08 + B04)/(B08 - B04)',
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L1C,
+                time_interval=(start_date, end_date),
+                mosaicking_order='leastCC'
+            )
+        ],
+        responses=[
+            SentinelHubRequest.output_response('default', 'png')
+        ],
+        bbox=bbox,
+        config=config
+    )
+
+    # Download images
+    download_client = SentinelHubDownloadClient(config=config)
+    request.download(download_client)
+
+    # Get image paths
+    images = []
+    for filename in os.listdir('sentinel_images'):
+        if filename.endswith('.png'):
+            images.append(os.path.join('sentinel_images', filename))
+    return images

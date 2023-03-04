2e5d66e2c83f531228b15f80bea5dfba7879c31b
Jeannie Jiang <jeanniej@usc.edu>
2023-03-04 11:56:10 -0800

    sentinel hub code

--- ghgsat_ai.py
+++
@@ -1,2 +0,0 @@
-# contain all the code needed to process the data received from the SentinelHub API and the GHGSat AI model
-# write functions to analyze the data and identify urban heat islands
--- green_roof_estimation.py
+++
@@ -1,2 +0,0 @@
-# estimate areas for incorporating green roofs and urban greenery
-# write functions to calculate the suitable areas for the green roofs and estimate the cost

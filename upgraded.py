3b4bb512c6e4dcf97bbfd43af487271eed7f75e4
Jashan Dhami <jdhami@usc.edu>
2023-03-04 18:49:38 -0800

    Add files via upload

--- upgraded.py
+++ upgraded.py
@@ -1,63 +1,30 @@
 import requests
 import json
-import sys
-import sqlite3
 
-# API keys
-open_ei_key = "NUlAemEybPtThidgm3mpPLnSpv6Cvp2YakXxztTY"
-airnow_key = "7697BBD5-B9C9-4BD0-94D1-86BC378944B7"
+# read input cities from file
+with open('cities.txt', 'r') as file:
+    cities = file.read().splitlines()
 
-# Input parameters
-cities_file = sys.argv[1]
-output_file = sys.argv[2]
+# open output file for writing
+with open('output.txt', 'w') as file:
+    # loop through cities and get carbon footprint info from OpenEI API
+    for city in cities:
+        # set API endpoint URL
+        url = f"https://api.openei.org/utility_rates?version=latest&format=json&api_key=NUlAemEybPtThidgm3mpPLnSpv6Cvp2YakXxztTY&sector=Residential&lat=40.7128&lon=-74.0060"
 
-# Connect to SQLite database
-conn = sqlite3.connect(output_file)
-c = conn.cursor()
+        # make API request
+        response = requests.get(url)
 
-# Create table to store data
-c.execute('''CREATE TABLE IF NOT EXISTS carbon_footprint 
-             (city text, state text, country text, 
-             energy_consumption real, waste_generated real, air_quality_index real)''')
+        # check if API request was successful
+        if response.status_code == 200:
+            # parse JSON response
+            data = json.loads(response.text)
 
-# OpenEI API URL
-open_ei_url = "https://api.openei.org/utility_rates?version=latest&format=json&lat={lat}&lon={lon}&api_key={key}"
+            # write carbon footprint info to output file
+            file.write(f"City: {city}\n")
+            file.write(f"Electricity carbon intensity: {data['electricity_carbon_intensity']}\n")
+            file.write(f"Natural gas carbon intensity: {data['natural_gas_carbon_intensity']}\n")
+            file.write(f"Total carbon intensity: {data['total_carbon_intensity']}\n\n")
+        else:
+            print(f"Failed to get carbon footprint data for {city}")
 
-# AirNow API URL
-airnow_url = "https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude={lat}&longitude={lon}&distance=25&API_KEY={key}"
-
-# Read cities from input file
-with open(cities_file, "r") as f:
-    cities = f.read().splitlines()
-
-# Loop over cities and retrieve data from APIs
-for city in cities:
-    print(f"Retrieving data for {city}...")
-
-    # Get latitude and longitude for city
-    geo_url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json"
-    response = requests.get(geo_url)
-    data = json.loads(response.text)
-    lat = data[0]["lat"]
-    lon = data[0]["lon"]
-
-    # Retrieve energy consumption and waste generation data from OpenEI API
-    open_ei_request = open_ei_url.format(lat=lat, lon=lon, key=open_ei_key)
-    response = requests.get(open_ei_request)
-    data = json.loads(response.text)
-    energy_consumption = data["items"][0]["total_energy"]
-    waste_generated = data["items"][0]["waste_generation_rate"]
-
-    # Retrieve air quality data from AirNow API
-    airnow_request = airnow_url.format(lat=lat, lon=lon, key=airnow_key)
-    response = requests.get(airnow_request)
-    data = json.loads(response.text)
-    air_quality_index = data[0]["AQI"]
-
-    # Add data to database
-    city_data = (city.split(",")[0], city.split(",")[1], "USA", energy_consumption, waste_generated, air_quality_index)
-    c.execute("INSERT INTO carbon_footprint VALUES (?, ?, ?, ?, ?, ?)", city_data)
-
-# Commit changes and close connection
-conn.commit()
-conn.close()
